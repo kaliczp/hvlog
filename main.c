d55f19ecc2b99784e0606fa23a10aa48c5f2bda4
Péter Kalicz <kaliczp@gmail.com>
2019-01-12 21:56:54 +0100

    TimeStamp checked by non-equality
    The timestamp which is saved into TimeRegister now compared to last
    saved (stored in backup register) with non equality.

--- main.c
+++ main.c
@@ -87,7 +87,7 @@ int main(void)
 		  MyStateRegister |= (STORE_TIMESTAMP_DAT | STORE_TIMESTAMP_TIM);
 		}
 	      /* Prevent debouncing, store same time */
-	      else if(RTC->BKP1R < TimeRegister)
+	      else if(RTC->BKP1R != TimeRegister)
 		{
 		  MyStateRegister |= STORE_TIMESTAMP_TIM;
 		}
