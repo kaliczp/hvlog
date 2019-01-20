2c80c933bd32419a603044bb059c819153b7252f
Péter Kalicz <kaliczp@gmail.com>
2019-01-20 11:29:15 +0100

    USART clock is MSE, RXNE interrupt cleared at USART disable
    LSE is not working as a clock source of USART. It is clocked now from
    MSE. The 1200 baud is kept for communication. Later possible to
    raise. The LSE selection is remained in the code.
    
    RXNE interrup after deconfiguration of USART remained active. Now it
    is cleared during USART deconfigure.

--- uart.c
+++ uart.c
@@ -71,7 +71,7 @@ void Configure_USART2(void)
   (void)tmpreg; /* (1) */
 
   /* Select LSE as USART2 clock source 11 */
-  RCC->CCIPR |= RCC_CCIPR_USART2SEL;
+  /* RCC->CCIPR |= RCC_CCIPR_USART2SEL; */
 
   GPIOB->MODER = (GPIOB->MODER & ~(GPIO_MODER_MODE6 | GPIO_MODER_MODE7)) \
     | (GPIO_MODER_MODE6_1 | GPIO_MODER_MODE7_1); /* (2) */
@@ -81,14 +81,14 @@ void Configure_USART2(void)
   RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
 
   /* Configure USART2 */
-  /* 32768 / 1200 = 27 */;
-  /* (1b) 1200 baud shifted right RM 0377 24.5.4 */
+  /* 2097152 / 1200 = 1748 */
+  /* (1b) 1200 baud RM 0377 24.5.4 */
   /* (2a) oversampling by 16 no OVER8, 8 data bit, 1 start bit, no
      parity, receive and receive interrupt enabled */
   /* (2aa) Set ONEBIT to increase the USART tolerance to timing deviations */
   /* (2ab) 2 stop bits STOP[1:0] = 10 */
   /* (2b) UART enabled with default values above */
-  USART2->BRR = 0b11011; /* (1) */
+  USART2->BRR = 0b11011010100; /* (1) */
   USART2->CR1 |= USART_CR1_RXNEIE | USART_CR1_RE ; /* (2a) */
   USART2->CR3 |= USART_CR3_ONEBIT; /* (2aa) */
   USART2->CR2 |= USART_CR2_STOP_1; /* (2ab) */
@@ -135,7 +135,7 @@ void Deconfigure_USART2(void)
   /* (5) Disable GPIOB clock */
   NVIC_DisableIRQ(USART2_IRQn); /* Disable USART2_IRQn */
   USART2->CR1 &= ~(USART_CR1_UE); /* (3) */
-  USART2->CR1 &= ~(USART_CR1_RE); /* (4) */
+  USART2->CR1 &= ~(USART_CR1_RE | USART_CR1_RXNEIE); /* (4) */
   GPIOB->MODER |= (GPIO_MODER_MODE6 | GPIO_MODER_MODE7); /* (4a) */
   RCC->APB1ENR &= ~(RCC_APB1ENR_USART2EN); /* (4b) */
   RCC->IOPENR &= ~(RCC_IOPENR_GPIOBEN); /* (5) */
